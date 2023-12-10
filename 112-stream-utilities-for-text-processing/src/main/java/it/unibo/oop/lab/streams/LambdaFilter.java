d572de38049735ff0fc6490faeacbdac109a7bee
Federico Mantoni <federico.mantoni@studio.unibo.it>
2023-12-10 17:00:36 +0100

    changed all lambdas in expression body

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -47,14 +47,14 @@ public final class LambdaFilter extends JFrame {
         COUNT_CHARS("Char counter", s -> String.valueOf(s.length())),
         COUNT_LINES("Lines counter", s -> String.valueOf(s.lines().count())),
         ALPHABETIC_ORDER("Sort word in alphabetical order", s -> {
-            final var list = Arrays.stream(s.split("[ \n]"));
-            return list.sorted()
+            return Arrays.stream(s.split("[ \n]"))
+                .sorted()
                 .reduce((w1, w2) -> w1 + "\n" + w2)
                 .orElse("");
         }),
         WORD_COUNT("Count equal words", s -> {
-            final var list = Arrays.stream(s.split("[ \n]"));
-            return list.collect(toMap(w -> w, c -> 1, Integer::sum))
+            return Arrays.stream(s.split("[ \n]"))
+                .collect(toMap(w -> w, c -> 1, Integer::sum))
                 .entrySet().stream()
                 .map(e -> e.getKey() + " -> " + e.getValue())
                 .reduce((w1, w2) -> w1 + "\n" + w2)
